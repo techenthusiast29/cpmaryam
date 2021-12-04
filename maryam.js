c9eb8afd79d389996520dd6340e041c9bc057247
techenthusiast29 <59942767+techenthusiast29@users.noreply.github.com>
2021-12-04 22:15:36 +0100

    Add files via upload

--- maryam.js
+++ maryam.js
@@ -25,13 +25,3 @@
          document.getElementById("time").innerHTML = "Happy Retirement Birthday";
      }
  }, 1000);
-
-
- $(document).ready(function() {
-     $(".scroll").pullToRefresh({
-             refresh: 200
-         })
-         .on("refresh.pulltorefresh", function() {
-             location.reload();
-         });
- });
